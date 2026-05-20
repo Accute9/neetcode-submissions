c7347ec1efd8f23ac49e5c3eb99f09d084da22be
Aneesh Mandapati <93799543+Accute9@users.noreply.github.com>
2026-05-20 12:22:36 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+
+        ## Time complexity: O(n) (technically O(n/2))
+        ## Space complexity: O(1), no dynamic memory is being allocated
